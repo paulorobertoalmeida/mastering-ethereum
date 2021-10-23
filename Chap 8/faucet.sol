99ca84f15506af046bc5615a4ca8823decb7614c
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-10-23 23:45:41 +0200

    Faucet part2

--- Chap 8/faucet.sol
+++ Chap 8/faucet.sol
@@ -1,9 +1,12 @@
+pragma solidity ^0.8.0;
 
 contract Faucet {
     function withdraw(uint withdraw_amount)public {
         require(withdraw_amount <= 100000000000000000);
 
-        msg.sender.transfer(withdraw_amount);
+        mmsg.sender.transfer(withdraw_amount);
+
+        receive () external payable {}
     }
     function () public payable {}
 }
